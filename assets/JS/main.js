ac14ab00fbc4b62e0d9b28997ad69bfd546cb63f
JOON617 <jjyt2525@gmail.com>
2023-12-06 17:47:10 +0900

    review section

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,4 +1,5 @@
 $(function(){
+    // GNB 
     $('.gnb > li > a').mouseenter(function(){
         $('header').addClass('active');
         $('.gnb li').children('.sub').removeClass('active');
@@ -8,4 +9,29 @@ $(function(){
         $('header').removeClass('active');
         $('.gnb li').children('.sub').removeClass('active');
     })
+
+    //review swiper
+    var reviewSwiper = new Swiper(".review-slide", {
+        slidesPerView: 1,
+        // spaceBetween: 10,
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+        noSwiping: true,
+        breakpoints: {
+            640: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            },
+            // 768: {
+            // slidesPerView: 4,
+            // spaceBetween: 40,
+            // },
+            1024: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+            },
+        },
+    });
 })
